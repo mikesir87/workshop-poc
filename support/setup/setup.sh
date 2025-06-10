18af98eef423d1653e462e1ffc37a71b667c052b
Michael Irwin <mikesir87@gmail.com>
2025-06-10 17:10:04 -0400

    Update the setup script to use an env var for the clone url

--- support/setup/setup.sh
+++ support/setup/setup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+if [ -z "$PROJECT_CLONE_URL" ]; then
+  echo "Error: PROJECT_CLONE_URL environment variable is not set."
+  exit 1
+fi
+
+if [ -d "/project/.git" ]; then
+  echo "Project already cloned. Exiting."
+  exit 0
+fi
+
 git config --global user.email "demo@example.com"
-git config --global user.name "Docker demo"
+git config --global user.name "Docker Workshop"
 git config --global --add safe.directory /project
 
 cd /project
@@ -9,6 +19,6 @@ cd /project
 # Remove everything including hidden files (like the .git directory)
 ls -A1 | xargs rm -rf
 
-git clone https://github.com/mikesir87/workshop-poc-content .
+git clone $PROJECT_CLONE_URL .
 
 chown 1000:1000 -R /project
